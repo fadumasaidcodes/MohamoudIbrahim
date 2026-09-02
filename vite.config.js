2d2d0d480fdbf85de576c82ab2f727aab3aae074
fadumasaidcodes <fadumasaid2022@gmail.com>
2026-09-02 04:34:57 +0100

    Set up GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ export default defineConfig({
   plugins: [react()],
   // If your repository is YOUR-USERNAME.github.io, use '/'.
   // If it is a project repository such as 'mohmud-portfolio', use '/mohmud-portfolio/'.
-  base: '/mohmud-portfolio/',
+  base: '/MohamoudIbrahim/',
+
 })
